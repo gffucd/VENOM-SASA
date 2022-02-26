fb61250f239c50c2e54febf1c459a060ae75990b
SASA-VENOM <100483822+SASA-VENOM@users.noreply.github.com>
2022-02-26 22:05:43 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -69,7 +69,7 @@ async def start_(client: Client, message: Message):
                 [InlineKeyboardButton("✨طريقة الاستخدام✨", callback_data="cbhowtouse")],
                 [
                     InlineKeyboardButton("✨الاوامر✨", callback_data="cbbasic"),
-                    InlineKeyboardButton("• المطور •", url=f"https://t.me/{OWNER_NAME}"),
+                    InlineKeyboardButton("✨المطور✨", url=f"https://t.me/{OWNER_NAME}"),
                 ],
                 [
                     ),
